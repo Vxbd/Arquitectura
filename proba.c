b1437ac9f7a56ae9e1b00a093a60834485b518ac
victorxbd <victorxesus.barreiro@rai.usc.es>
2020-02-20 19:25:12 +0100

    Codigo decente, sen medicion de tempos.

--- proba.c
+++ proba.c
@@ -1,3 +1,9 @@
+/*
+ * Autores: Victor Xesus Barreiro Dominguez e Helena Castro Castreje
+ * Titulo: Codigo preliminar para a realización de probas. Práctica ArqComp
+ * Descripcion: Programa para analise de xerarquia de memoria. 
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,12 +11,10 @@
 int main(int argc, char const *argv[])
 {
 
-
     int D; //Tamanho vector interno
     int R; //Elementos lista, numero de estruturas.
     long int k; //Elemento auxiliar para asignacion a unha variable
 
-    printf("Antes de atoi\n");
     //Asinacion de D e R por linha de comandos
     if(argc == 3){
         D = atoi(argv[1]);
@@ -19,22 +23,19 @@ int main(int argc, char const *argv[])
 
     } else{
         printf("ERRO: O formato debe ser: ./executable -D -R\n");
-        //return 0;
+        return 0;
     }
 
     typedef struct s {
         struct s *p;
-        long int data[20000];
+        long int data[D];
     } s;
-
-    R = 1000;
-
-    printf("hfosaf\n");
 
     //Creamos a lista e reservamos memoria
     s *punteirosLista = malloc (sizeof(s) * R);
 
     for (int i = 0; i < R-1; i++) {
+        printf("%p poscion\n, ", &(punteirosLista[0].data[0]));
         punteirosLista[i].p = &punteirosLista[i+1];
         printf(" %d\n", i);
         /*
@@ -42,20 +43,15 @@ int main(int argc, char const *argv[])
          * consideramos q ue podería contaminar as probas
         */
     }
-    printf("hfosaf3\n");
-
 
     //Circular
     punteirosLista[R -1].p = &punteirosLista[0];
-
 
     //Accedemos cun bucle
     for (int j = 0; j < R; j++) {
         k = punteirosLista[j].p->data[0];
         printf("Esto anda funsionando webon\n");
     }
-
-printf("Las mejores cosas siempre tienen un fin, pero el recuerdo siempre perduraran en nuestros corasones\n");
 
     return 0;
 }
