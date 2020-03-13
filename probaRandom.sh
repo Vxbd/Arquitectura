039b12197feeee1b253ad58aedc72914c00d72b9
helenacastr <34631581+helenacastr@users.noreply.github.com>
2020-03-13 11:51:08 +0100

    Create probaRandom.sh

---
+++ probaRandom.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script pruebas de Arquitectura de Computadores
+
+# Caches
+s1=32768 # Empleamos solo la parte de datos
+
+s2=262144
+
+s3=6291456
+
+NLC=($(echo 0.5*$s1 | bc) $(echo 1.5*$s1 | bc) $(echo "0.5*$s2" | bc) \
+$(echo "1.5*$s2" | bc) $(echo "0.5*$s3" | bc) $(echo "0.75*$s3" | bc) \
+$(echo "2*$s3" | bc) $(echo "4*$s3" | bc) $(echo "8*$s3" | bc))
+D=(1 3 15 40 80)
+
+echo "${D[@]}"
+
+for i in "${NLC[@]}";do
+	for j in "${D[@]}";do
+		echo $i
+		R=$(echo "$i/(8+(8*$j))" | bc)
+		echo "Prueba -> ./arcompRandom $j $R"
+		./arcompRandom $j $R
+	done
+done
